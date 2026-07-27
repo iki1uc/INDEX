7afbd8dd69cde2a063e8ea97860db0a8ca2eca3b
iki1uc <entertrainer@web.de>
2026-07-27 05:48:06 +0200

    Update INDEX.respo.js

--- core/INDEX.respo.js
+++ core/INDEX.respo.js
@@ -1,21 +1,18 @@
 export const INDEX_RESPO = {
 
-    // 380 Karos erzeugen
     build(){
         const karo = [];
 
         for(let i=0; i<380; i++){
+            const r = i % 9;
+            const c = Math.floor(i / 9);
+
             karo.push({
                 id: i,
-
-                // Qi / IQQ aus VECTOR
-                qi: VECTOR.qi(i % 9, Math.floor(i / 9)),
-                iqq: VECTOR.iqq(i % 9, Math.floor(i / 9)),
-
-                // Gamma aus GAM
+                qi: VECTOR.qi(r, c),
+                iqq: VECTOR.iqq(r, c),
                 gamma: GAM.gamma(),
-
-                // Platzhalter für Score
+                cube: VECTOR_CUBEMIND.evaluate(i),
                 score: 0
             });
         }
