95266a53735fc3b7758e5b5c414ca62b35ed5574
iki1uc <entertrainer@web.de>
2026-07-27 06:34:09 +0200

    Add initial matrix.js with chess piece representation

---
+++ M/matrix.js
@@ -0,0 +1,4 @@
+♕:green  ♘:yellow  ♙:red  ♗:green  ♖:yellow  ♙:red  ♔:green  ♘:yellow  ♙:red
+♙:red    ♗:yellow  ♘:yellow  ♙:red  ♕:green  ♖:yellow  ♙:red  ♘:yellow  ♙:red
+...
+♘:yellow ♙:red    ♖:yellow  ♕:green  ♙:red  ♗:yellow  ♙:red  ♘:yellow  ♔:green
