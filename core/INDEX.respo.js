d2199fe2f671758823a0981ecd99f9c0d81cb9fe
iki1uc <entertrainer@web.de>
2026-07-27 05:37:09 +0200

    Update INDEX.respo.js

--- core/INDEX.respo.js
+++ core/INDEX.respo.js
@@ -1,1 +1,19 @@
+export const RESPO_INDEX = {
+    ALL: {},
+    ORG: {},
+    LOL: {},
+    PIPE: [],
+    META: {},
+    LOAD: {},
 
+    async init() {
+        const mega = await import("./MEGA_INDEX.js");
+
+        this.ALL  = mega.MEGA.ALL;
+        this.ORG  = mega.MEGA.ORG;
+        this.LOL  = mega.MEGA.LOL;
+        this.PIPE = mega.MEGA.PIPE;
+        this.META = mega.MEGA.META;
+        this.LOAD = mega.MEGA.LOAD;
+    }
+};
