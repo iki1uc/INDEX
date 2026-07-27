5ea28053c31ca2c19e7743eea0a1b18ba23a7e8b
iki1uc <entertrainer@web.de>
2026-07-27 05:46:18 +0200

    Update INDEX.respo.js

--- core/INDEX.respo.js
+++ core/INDEX.respo.js
@@ -1,19 +1,27 @@
-export const RESPO_INDEX = {
-    ALL: {},
-    ORG: {},
-    LOL: {},
-    PIPE: [],
-    META: {},
-    LOAD: {},
+export const INDEX_RESPO = {
 
-    async init() {
-        const mega = await import("./MEGA_INDEX.js");
+    // 380 Karos erzeugen
+    build(){
+        const karo = [];
 
-        this.ALL  = mega.MEGA.ALL;
-        this.ORG  = mega.MEGA.ORG;
-        this.LOL  = mega.MEGA.LOL;
-        this.PIPE = mega.MEGA.PIPE;
-        this.META = mega.MEGA.META;
-        this.LOAD = mega.MEGA.LOAD;
+        for(let i=0; i<380; i++){
+            karo.push({
+                id: i,
+
+                // Qi / IQQ aus VECTOR
+                qi: VECTOR.qi(i % 9, Math.floor(i / 9)),
+                iqq: VECTOR.iqq(i % 9, Math.floor(i / 9)),
+
+                // Gamma aus GAM
+                gamma: GAM.gamma(),
+
+                // Platzhalter für Score
+                score: 0
+            });
+        }
+
+        return karo;
     }
 };
+
+window.INDEX_RESPO = INDEX_RESPO;
